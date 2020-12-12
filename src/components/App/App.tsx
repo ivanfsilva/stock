9e2c4ebb9aed2ff2279537f9d95db012a24024de
ivanfsilva <ivanfs27@gmail.com>
2020-12-12 14:33:43 -0300

    preparação tabelas

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import Button from '../Button';
-import Header from '../Header';
 import './App.css';
+import Header from '../Header';
+import Container from '../../shared/Container';
 
 function App() {
   return (
     <div className="App">
-      <Header title="Stock"/>
-
-      <div className="Container">
-        <Button 
-          onClick={() => window.alert('TESTE')} 
-        > 
-          Batata
-        </Button>
-      </div>
+      <Header title="Stock" />
+      <Container>
+    
+      </Container>
     </div>
-
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from 'react'
+import './Header.css'
 
 declare interface HeaderProps {
-    title: string;
+  title: string
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
-    return <header className="AppHeader">
-        <h1>{ props.title }</h1>
-    </header>
+  return <header className="AppHeader">
+    <h1>{ props.title }</h1>
+  </header>
 }
 
-export default Header;
+export default Header
